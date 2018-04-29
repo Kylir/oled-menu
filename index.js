4df8729a7011bb715b14f32d53e3955a575c5d63
Kylir <jason.forest@glyphe.org>
2018-04-29 01:00:32 +0100

    Load font

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const ssd1306 = require('ssd1306-i2c-js')
 const display = ssd1306.display
 const Font = ssd1306.Font
-const Color = ssd1306.Color
-const Layer = ssd1306.Layer
 
 process.on('SIGINT', function(){
     display.dispose()
@@ -13,7 +11,8 @@ display.init(1, 0x3C)      // Open bus and initialize driver
 display.turnOn()           // Turn on display module
 display.clearScreen()      // Clear display buffer
 
+display.setFont(Font.UbuntuMono_8ptFontInfo);
 // display.drawString(x:number, y:number, text, size, color, layer)
-display.drawString(0, 0, 'A text on the screen')
+display.drawString(0, 0, 'A text on the screen', 1, 1, 0)
 
 display.refresh()          // Write buffer in display registries
